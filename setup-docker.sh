91ad0f423d20f06a343c5a4fb52d904a2055c750
morington <morington.mail@gmail.com>
2024-10-15 12:53:06 +0300

    Update docker setup script

--- setup-docker.sh
+++ setup-docker.sh
@@ -44,7 +44,7 @@ show_header
 # Запуск команды для каждого этапа
 log_step "Обновляем список пакетов" "sudo apt-get update"
 log_step "Устанавливаем пакеты для работы через HTTPS" "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common"
-log_step "Добавляем GPG-ключ репозитория Docker" "bash -c 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'"
+log_step "Добавляем GPG-ключ репозитория Docker" "bash -c \"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -\""
 log_step "Добавляем репозиторий Docker в источники APT" 'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"'
 log_step "Обновляем список пакетов после добавления репозитория Docker" "sudo apt-get update"
 log_step "Проверяем доступные версии Docker" "apt-cache policy docker-ce"
