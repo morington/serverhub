690769ae036cbf5c2c882ea506f7b4ee35a15bae
morington <morington.mail@gmail.com>
2024-10-15 13:11:32 +0300

    Update docker setup script

--- setup-docker.sh
+++ setup-docker.sh
@@ -23,12 +23,18 @@ RESET='\e[0m'
 log_step() {
   local message=$1
   local command=$2
+  local interactive=${3:-false} # Флаг для интерактивных команд
 
   # Выводим сообщение с "идет выполнение" и очищаем строку
   printf "${YELLOW}%-60s${RESET}" "$message... "
 
-  # Выполняем команду с подавлением вывода
-  eval "$command"
+  # Выполняем команду
+   if [ "$interactive" = true ]; then
+    eval "$command"
+  else
+    # Выполняем команду, подавляя её вывод
+    eval "$command" >/dev/null 2>&1
+  fi
 
   # Проверка успешности выполнения команды
   if [ $? -eq 0 ]; then
@@ -45,12 +51,12 @@ show_header
 # Запуск команды для каждого этапа
 log_step "Обновляем список пакетов" "sudo apt-get update"
 log_step "Устанавливаем пакеты для работы через HTTPS" "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common"
-log_step "Добавляем GPG-ключ репозитория Docker" "bash -c \"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -\""
+log_step "Добавляем GPG-ключ репозитория Docker" "bash -c \"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -\"" true
 log_step "Добавляем репозиторий Docker в источники APT" 'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"'
 log_step "Обновляем список пакетов после добавления репозитория Docker" "sudo apt-get update"
 log_step "Проверяем доступные версии Docker" "apt-cache policy docker-ce"
 log_step "Устанавливаем Docker" "sudo apt-get install -y docker-ce"
-log_step "Проверка статуса Docker" "sudo systemctl status docker --no-pager"
+log_step "Проверка статуса Docker" "sudo systemctl status docker --no-pager" true
 
 # Запрашиваем пользователя о добавлении в группу Docker
 CURRENT_USER=$(whoami)
