6fbc3f160a13c146d52bd5699db52d1bb53a8fb7
morington <morington.mail@gmail.com>
2024-10-15 13:22:13 +0300

    Update docker setup script

--- setup-docker.sh
+++ setup-docker.sh
@@ -23,7 +23,7 @@ RESET='\e[0m'
 log_step() {
   local message=$1
   local command=$2
-  local interactive=${3:-false} # Флаг для интерактивных команд
+  local interactive=${3:-true} # Флаг для интерактивных команд
 
   # Выводим сообщение с "идет выполнение" и очищаем строку
   printf "${YELLOW}%-60s${RESET}" "$message... "
@@ -51,12 +51,25 @@ show_header
 # Запуск команды для каждого этапа
 log_step "Обновляем список пакетов" "sudo apt-get update"
 log_step "Устанавливаем пакеты для работы через HTTPS" "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common"
-log_step "Добавляем GPG-ключ репозитория Docker" "bash -c \"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -\"" true
-log_step "Добавляем репозиторий Docker в источники APT" 'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"'
+log_step "Добавляем GPG-ключ репозитория Docker" "bash -c \"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -\""
+log_step "Добавляем репозиторий Docker в источники APT" 'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"' true
 log_step "Обновляем список пакетов после добавления репозитория Docker" "sudo apt-get update"
-log_step "Проверяем доступные версии Docker" "apt-cache policy docker-ce"
+
+if apt-cache policy docker-ce | grep -q "Candidate:"; then
+    echo -e "${GREEN}Пакет `docker-ce` доступен в репозиториях."
+else
+    echo -e "${RED}Ошибка: ${YELLOW}Пакет `docker-ce` недоступен."
+    exit 1
+fi
+
 log_step "Устанавливаем Docker" "sudo apt-get install -y docker-ce"
-log_step "Проверка статуса Docker" "sudo systemctl status docker --no-pager" true
+
+if systemctl is-active --quiet ssh; then
+    echo -e "${GREEN} запущен."
+else
+    echo -e "${RED}Ошибка: ${YELLOW}Docker не запущен."
+    exit 1
+fi
 
 # Запрашиваем пользователя о добавлении в группу Docker
 CURRENT_USER=$(whoami)
