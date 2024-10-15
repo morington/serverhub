b5a13aaf9b14c86dc656f404727550700c890c53
morington <morington.mail@gmail.com>
2024-10-15 13:35:21 +0300

    Update docker setup script

--- setup-docker.sh
+++ setup-docker.sh
@@ -52,7 +52,13 @@ show_header
 
 # Запуск команды для каждого этапа
 log_step "Обновляем список пакетов" "sudo apt-get update"
-log_step "Устанавливаем пакеты для работы через HTTPS" "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common"
+
+if systemctl is-active --quiet ssh; then
+    echo -e "${YELLOW}Docker уже установлен."
+    exit 1
+fi
+
+log_step "Устанавливаем пакеты для работы через HTTPS" "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common"
 log_step "Добавляем GPG-ключ репозитория Docker" "bash -c \"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -\""
 log_step "Добавляем репозиторий Docker в источники APT" 'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"' true
 log_step "Обновляем список пакетов после добавления репозитория Docker" "sudo apt-get update"
